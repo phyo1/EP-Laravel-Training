70b14533c827f05e80f78c7b2926cd5a53ef9244
phyo1 <eiphyolin@gmail.com>
2015-09-27 10:38:31 +0630

    Update project including curl and migration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,38 @@ Route::get('/about', function () {
 Route::get('/','PagesController@home');
 Route::get('/about/{name}','PagesController@about');
 Route::get('/contact','PagesController@contact');
+
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+use \App\User;
+use \App\Blog;
+
+Route::get('/user',function(){
+	dd(User::all());
+});
+Route::get('/blog/create',function(){
+	$blog = new Blog;
+	$blog->name= "First Blog";
+	$blog->description= "First Blog";
+	$blog->save();
+});
+
+Route::get('/blog',function(){
+	return Blog::all();
+});
+
+Route::get('/blog/{id}',function($id){
+	$blog = Blog::find($id);
+	return $blog;
+});
+Route::get('/blog/t/{tit}',function($tit){
+	$blog = Blog::where('name', "=", $tit)->get();
+	return $blog;
+});
+
